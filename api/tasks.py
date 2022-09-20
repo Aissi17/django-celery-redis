1b94f9ae11718c7c2aa457f6386a74698bbf927d
Islam <aissiislam97@gmail.com>
2022-09-20 08:20:35 +0100

    added celery progress

--- api/tasks.py
+++ api/tasks.py
@@ -2,6 +2,8 @@ import random
 from celery import shared_task
 import time
 from .models import SalesForecast
+import logging
+from celery_progress.backend import ProgressRecorder
 
 
 @shared_task
@@ -15,8 +17,19 @@ def mul(x, y):
     return total
 
 
+@shared_task(bind=True)
+def go_to_sleep(self, duration):
+    progress_recorder = ProgressRecorder(self)
+    for i in range(5):
+        time.sleep(duration)
+        progress_recorder.set_progress(i + 1, 5, f"on iteration {i}")
+    time.sleep(duration)
+    return "Done"
+
+
 @shared_task
 def get_sf_data(sku):
+    logging.warning('The task "SF" is running !')
     time.sleep(5)
 
     obj = SalesForecast.objects.create(
@@ -25,4 +38,5 @@ def get_sf_data(sku):
         price_value=random.uniform(0, 1000),
     )
 
+    logging.info("Task finished")
     return sku
